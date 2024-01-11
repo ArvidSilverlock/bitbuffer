4f7a0692d982af7656f099dd0f3c850a98e957d5
Arvid <ArvidSilverlock@gmail.com>
2024-01-11 11:33:44 +0000

    implemented lua iteration functions to iterate over certain chunks of numbers

---
+++ src/BitIterate.lua
@@ -0,0 +1,34 @@
+local U24_BITS = 24
+local U32_BITS = 32
+
+local function normal(width: number, bit: number)
+	local chunkWidth = if bit % 8 == 0 then U32_BITS else 8 - bit
+	local position = 0
+
+	return function()
+		if chunkWidth > 0 then
+			local previousPosition, previousChunkWidth = position, chunkWidth
+
+			position += chunkWidth
+			chunkWidth = math.min(width - position, U32_BITS)
+
+			return previousPosition, previousChunkWidth
+		end
+	end
+end
+
+local function flipped(width: number, bit: number)
+	return function(_, position)
+		if position > 0 then
+			local chunkWidth = math.min(position, U24_BITS)
+			return position - chunkWidth, chunkWidth
+		end
+	end,
+		nil,
+		width
+end
+
+return {
+	normal = normal,
+	flipped = flipped,
+}
