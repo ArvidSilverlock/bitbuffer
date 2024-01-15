da0c9506a6baca30908abe823df022194ff01e3e
Arvid <ArvidSilverlock@gmail.com>
2024-01-15 22:49:31 +0000

    update test grabbing

--- tests/init.lua
+++ tests/init.lua
@@ -1,22 +1,3 @@
-local function merge<T>(...: { T }): { T }
-	local count = select("#", ...)
-	assert(count >= 2, "there must be two or more tables to merge")
-
-	local initial = select(1, ...)
-	local output = table.clone(initial)
-
-	for i = 2, count do
-		local value = select(i, ...)
-		table.move(value, 1, #value, #output + 1, output)
-	end
-
-	return output
-end
-
-local function collapse<T>(t: { { T } }): { T }
-	return merge(table.unpack(t))
-end
-
 local function cull<K, V>(input: { [K]: V }, predicate: (value: V, key: K) -> boolean?): { [K]: V }
 	local output = table.clone(input)
 
@@ -29,66 +10,6 @@ local function cull<K, V>(input: { [K]: V }, predicate: (value: V, key: K) -> bo
 	return output
 end
 
-local function cullDuplicates<V>(input: { V }): { V }
-	local output = {}
-	local found = {}
-
-	for _, value in input do
-		if not found[value] then
-			table.insert(output, value)
-			found[value] = true
-		end
-	end
-
-	return output
-end
-
-local function map<K, V>(input: { [K]: V }, transformer: (value: V, key: K) -> boolean?): { [K]: V }
-	local output = {}
-
-	for key, value in input do
-		output[key] = transformer(value, key)
-	end
-
-	return output
-end
-
-local function compareTests(a: ModuleScript, b: ModuleScript): boolean
-	local numberA, numberB = a.Name:match("(%d+)%.spec$"), b.Name:match("(%d+)%.spec$")
-	return if numberA and numberB
-		then tonumber(numberA) < tonumber(numberB)
-		elseif numberA and not numberB then false
-		elseif numberB and not numberA then true
-		else a.Name < b.Name
-end
-
-local Editors = script.Editors
-
--- Really ugly, but sorts the tests in the correct order
--- stylua: ignore
-return cullDuplicates(cull(collapse(map(
-	{
-		{
-			script["littleEndian.spec"],
-			script["bigEndian.spec"],
-			script["tobase.spec"],
-			script["frombase.spec"]
-		},
-		Editors.UInt,
-		Editors.Int,
-		Editors.Float,
-		Editors
-	},
-	function(value) -- map
-		if typeof(value) == "Instance" and value:IsA("Folder") then
-			local sortedChildren = value:GetChildren()
-			table.sort(sortedChildren, compareTests)
-			return sortedChildren
-		else
-			return value
-		end
-	end)),
-	function(value) -- cull
-		return value:IsA("ModuleScript")
-	end)
-)
+return cull(script:GetDescendants(), function(value)
+	return value:IsA("ModuleScript")
+end)
